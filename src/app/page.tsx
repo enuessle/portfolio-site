0f1a7d526545b98f86c460859667cacd97963b4b
Ethan Nuessle <ethan.nuessle@gmail.com>
2024-03-01 15:05:59 -0500

    css changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import Tabs from "@/components/tabs/tabs";
 export default function Home() {
   return (
     <div className={styles["main"]}>
-      <h1>Ethan Nuessle</h1>
+      <h1 className={styles["title"]}>Ethan Nuessle</h1>
       <Tabs />
     </div>
   );
