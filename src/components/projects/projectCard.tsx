899357e581911253d9a6b140a27112735d0c6c30
enuessle <77037513+enuessle@users.noreply.github.com>
2024-03-05 20:48:23 -0500

    Project Cards Templated

--- src/components/projects/projectCard.tsx
+++ src/components/projects/projectCard.tsx
@@ -1,16 +1,31 @@
 "use client";
 
+import {
+  ReactElement,
+  JSXElementConstructor,
+  ReactNode,
+  ReactPortal,
+  PromiseLikeOfReactNode,
+} from "react";
 import styles from "./projects.module.css";
 
+interface projectDetails {
+  name?: string;
+  links?: string;
+  image?: string;
+  details?: string;
+  tech?: string;
+}
+
 //TODO: Add Scalable Projects Section Blocks
-export default function ProjectCard() {
+export default function ProjectCard(props: projectDetails) {
   return (
     <div className={styles["card"]}>
-      <h4>Project Name</h4>
-      <h5>Links</h5>
-      <h4>Image</h4>
-      <p>Project Details</p>
-      <p>Technical Details</p>
+      <h4>{props.name}</h4>
+      <h5>{props.links}</h5>
+      <img className={styles["image"]} src={props.image}></img>
+      <p>{props.details}</p>
+      <p>{props.tech}</p>
     </div>
   );
 }
--- src/components/projects/projects.tsx
+++ src/components/projects/projects.tsx
@@ -9,8 +9,14 @@ export default function Projects() {
     <div>
       <h1> WIP: Projects</h1>
       <div className={styles["row"]}>
-        <ProjectCard />
-        <ProjectCard />
+        <ProjectCard
+          name="Project Name"
+          links="Links"
+          image="https://raw.githubusercontent.com/GameXplode/Chip8/main/pics/TETRIS.png"
+          details="Stuff"
+          tech="Tech"
+        />
+        <ProjectCard name="Project 2 Name" />
       </div>
     </div>
   );
