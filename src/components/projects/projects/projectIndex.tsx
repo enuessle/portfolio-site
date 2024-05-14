47a43ab114e53e24b0f91d92f815f247d46b8f45
Ethan Nuessle <ethan.nuessle@gmail.com>
2024-05-14 00:21:22 -0400

    Static nextjs

--- src/components/projects/projects/projectIndex.tsx
+++ src/components/projects/projects/projectIndex.tsx
@@ -1,3 +1,3 @@
 export { default as Chip8 } from "./chip8.json";
-export { default as Test } from "./test.json";
 export { default as OsuExchange } from "./osu-exchange.json";
+export { default as VZCode } from "./vzcode.json";
