91881766a9f9357c5f4591d9923f501a90bb6236
Ethan Nuessle <ethan.nuessle@gmail.com>
2025-07-09 22:05:04 -0400

    Visual Arithmetech

--- src/components/projects/projects/projectIndex.tsx
+++ src/components/projects/projects/projectIndex.tsx
@@ -1,3 +1,4 @@
 export { default as Chip8 } from "./chip8.json";
 export { default as OsuExchange } from "./osu-exchange.json";
 export { default as VZCode } from "./vzcode.json";
+export { default as VisualArithmetech } from "./visual-arithmetech.json";
