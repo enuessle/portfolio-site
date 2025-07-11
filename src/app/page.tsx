c3b841cc87dba49cf326504bcf22ce2808b93878
Ethan Nuessle <ethan.nuessle@gmail.com>
2025-07-10 21:13:02 -0400

    Text Highlights

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,15 +7,22 @@ export default function Home() {
     <div className={styles["main"]}>
       <h1 className={styles["title"]}>Ethan Nuessle</h1>
       <p className={styles["aboutMe"]}>
-        Hi! I'm Ethan, a B.S. Computer Science and Mathematics graduate from
-        Rensselaer Polytecnic Institute! I have a strong academic background,
-        graduating Magna Cum Laude with a 3.89 GPA and a minor in Cognitive
-        Science, and also have a large breadth of experience contributing to
-        Open-Source software, working with teams at IMB to contribute to the
-        Backends of Ceph and Tornjak, and with VizHub on Full-Stack
-        contributions to VZCode. I'm passionate about Software Engineering and
-        Artificial Intelligence, and the how these new technological
-        advancements can change the current tech landscape!
+        Hi! I'm Ethan, a{" "}
+        <span className={styles["altColour"]}>
+          B.S. Computer Science and Mathematics
+        </span>{" "}
+        graduate from Rensselaer Polytecnic Institute! I have a strong academic
+        background, graduating Magna Cum Laude with a 3.89 GPA and a{" "}
+        <span className={styles["altColour"]}>minor in Cognitive Science</span>,
+        and also have a large breadth of experience contributing to Open-Source
+        software, working with teams at IMB to contribute to the Backends of
+        Ceph and Tornjak, and with VizHub on Full-Stack contributions to VZCode.
+        I'm passionate about{" "}
+        <span className={styles["altColour"]}>
+          Software Engineering and Artificial Intelligence
+        </span>
+        , and the how these new technological advancements can change the
+        current tech landscape!
       </p>
       <div className={styles["linkRow"]}>
         <Link
