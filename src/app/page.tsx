ecd0cc1bd9efd15c738ea00bd7f3b10cc4f75512
Ethan Nuessle <ethan.nuessle@gmail.com>
2025-07-10 20:48:35 -0400

    Vertical Sizing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,13 +9,13 @@ export default function Home() {
       <p className={styles["aboutMe"]}>
         Hi! I'm Ethan, a B.S. Computer Science and Mathematics graduate from
         Rensselaer Polytecnic Institute! I have a strong academic background,
-        graduating Magna Cum Laude with a 3.89 GPA, with an additional minor in
-        Cognitive Science, and also have a large breadth of experience
-        contributing to Open-Source software, working with teams at IMB to
-        contribute to the Backends of Ceph and Tornjak, and with VizHub on
-        Full-Stack contributions to VZCode. I'm passionate about Software
-        Engineering and Artificial Intelligence, and the how these new
-        technological advancements can change the current landscape!
+        graduating Magna Cum Laude with a 3.89 GPA and a minor in Cognitive
+        Science, and also have a large breadth of experience contributing to
+        Open-Source software, working with teams at IMB to contribute to the
+        Backends of Ceph and Tornjak, and with VizHub on Full-Stack
+        contributions to VZCode. I'm passionate about Software Engineering and
+        Artificial Intelligence, and the how these new technological
+        advancements can change the current tech landscape!
       </p>
       <div className={styles["linkRow"]}>
         <Link
